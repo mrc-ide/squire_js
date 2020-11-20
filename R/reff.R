2082c5d625006c2b2c71efbf582a95ea2c5d0f84
Giovanni Charles <giovanni.charles@gmail.com>
2020-11-20 11:07:49 +0000

    Bugs fixed on reff run

--- R/reff.R
+++ R/reff.R
@@ -1,4 +1,4 @@
-get_immunity_ratios <- function(out, Rt) {
+get_immunity_ratios <- function(out, Rt, t_now) {
 
   # mixing_matrix is already the mixing matrix that we pass to you in the country json files
   mixing_matrix <- squire:::process_contact_matrix_scaled_age(
@@ -17,43 +17,30 @@ get_immunity_ratios <- function(out, Rt) {
   # pop is a 17 length with population sizes in each age category
   pop <- out$parameters$population
 
-  # here we loop over each replicate (no need to worry about that for covidsim
-  prop_susc <- lapply(seq_len(dim(out$output)[3]), function(x) {
-
-    # in here we work out each time point the number of idnividuals in each age category in
-    # the S compartment and then divide by the total population size in each age category
-    # to give the proportion susceptible at each time point
-    t(t(out$output[, index$S, x])/pop)
-  } )
+  # in here we work out each time point the number of idnividuals in each age category in
+  # the S compartment and then divide by the total population size in each age category
+  # to give the proportion susceptible at each time point
+  prob_susc <- t(t(out$output[, index$S,])/pop)
 
   # Length 17 with relative R0 in each age category
   relative_R0_by_age <- prob_hosp*dur_ICase + (1-prob_hosp)*dur_IMild
 
-  # here we loop over each replicate (no need to worry about that for covidsim)
-  adjusted_eigens <- lapply(prop_susc, function(x) {
-
-    # here we are looping over each time point to calculate the adjusted eigen
-    # incorporating the proportion of the suscpetible population in each age group
-    # (below this is the x[y,] term)
-    unlist(lapply(seq_len(nrow(x)), function(y) {
-      if(any(is.na(x[y,]))) {
-        return(NA)
-      } else {
-        Re(eigen(mixing_matrix*x[y,]*relative_R0_by_age)$values[1])
-      }
-    }))
-  })
+  # here we are looping over each time point to calculate the adjusted eigen
+  # incorporating the proportion of the suscpetible population in each age group
+  adjusted_eigens <- vapply(
+    seq(t_now),
+    function(t) {
+      Re(eigen(mixing_matrix * prob_susc[t,] * relative_R0_by_age)$values[1])
+    },
+    numeric(1)
+  )
 
   # multiply beta by the adjusted eigen at each time point and devide by the
   # original R0
-  ratios <- lapply(seq_along(adjusted_eigens), function(x) {
-    (out$parameters$beta_set[[1]] * adjusted_eigens[[x]]) / out$parameters$R0[[1]]
-  })
+  ratios <- (out$parameters$beta_set * adjusted_eigens) / out$parameters$R0
 
   # multiply our time varying Rt by the ratios at each time point to get Reff
-  Reff <- lapply(seq_along(ratios), function(x) {
-    Rt * x
-  })
+  Reff <- Rt * ratios
 
   return(Reff)
 }
